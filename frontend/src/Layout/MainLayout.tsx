743fde8d4d5b37bb47bed5771db5807c368216fc
Siddharth Chhatbar <siddharth3901@gmail.com>
2025-06-30 07:46:44 +0900

    sidebar state cookie

--- frontend/src/Layout/MainLayout.tsx
+++ frontend/src/Layout/MainLayout.tsx
@@ -3,16 +3,19 @@ import Navbar from "../components/Navbar";
 import { SidebarProvider } from "../components/ui/sidebar";
 import { AppSidebar } from "../components/app-sidebar";
 
-// function getCookieValue(name: string) {
-//   return document.cookie
-//     .split("; ")
-//     .find((row) => row.startsWith(name + "="))
-//     ?.split("=")[1];
-// }
+function getCookieValue(name: string) {
+  return document.cookie
+    .split("; ")
+    .find((row) => row.startsWith(name + "="))
+    ?.split("=")[1];
+}
 
 const MainLayout = () => {
+  const cookieStore = getCookieValue("sidebar_state");
+  const defaultOpen = cookieStore === "true";
+
   return (
-    <SidebarProvider>
+    <SidebarProvider defaultOpen={defaultOpen}>
       <AppSidebar variant="sidebar" />
       <div className="w-full h-full">
         <Navbar />
