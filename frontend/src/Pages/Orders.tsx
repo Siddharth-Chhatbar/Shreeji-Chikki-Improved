960a8cfd9e2ae651031c12e8961a75c685072317
Siddharth Chhatbar <siddharth3901@gmail.com>
2025-07-07 07:53:54 +0900

    orders relocate

--- frontend/src/Pages/Orders.tsx
+++
@@ -1,7 +0,0 @@
-const Orders = () => {
-  // Make different column file for orders as 
-  // it needs filtering based on a orders delivery status
-  return <div>Orders</div>;
-};
-
-export default Orders;
