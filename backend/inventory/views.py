f6ac22069da29b4d128433ef3cbbf3f455ba56a0
Siddharth Chhatbar <siddharth3901@gmail.com>
2025-08-01 07:07:16 +0900

    update inventory views

--- backend/inventory/views.py
+++ backend/inventory/views.py
@@ -13,12 +13,3 @@ class InventoryViewSet(ModelViewSet):
     serializer_class = InventoryPolymorphicSerializer
     lookup_field = "id"
     permission_classes = []
-
-    def get_serializer_class(self):
-        instance = self.get_object()
-
-        if isinstance(instance, ProductInventory):
-            return ProductInventorySerializer
-        elif isinstance(instance, RawMaterialInventory):
-            return RawMaterialInventorySerializer
-        return InventorySerializer
